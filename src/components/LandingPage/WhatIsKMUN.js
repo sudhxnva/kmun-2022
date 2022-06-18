a5d6ea4e5319e1c1a01163386eaa02da1ea3a975
Sudhanva M <sudhanva.m@pm.me>
2022-06-19 00:43:06 +0530

    Add image to what is KMUN section

--- src/components/LandingPage/WhatIsKMUN.js
+++ src/components/LandingPage/WhatIsKMUN.js
@@ -2,29 +2,43 @@ import React from "react"
 import ButtonFilled from "../Button/ButtonFilled"
 import { BasicTextModuleStyles } from "../BasicTextModule/BasicTextModuleStyles"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import "./LandingPageStyles.css"
 
 const WhatIsKMUN = ({ title, content, link, linkText }) => {
   return (
     <BasicTextModuleStyles className="section">
-      <div className="container container__tight">
-        <div>
-          <h1 style={{ fontSize: "80px", fontWeight: "900" }}>
-            WHAT IS <span style={{ fontSize: "160px" }}>KMUN</span>
+      <div className="container container__tight kmun-inner-container">
+        <div className="kmun-desc-inner-container">
+          <h1 className="kmun-desc-heading">
+            WHAT IS <span>KMUN</span>
           </h1>
-          <p style={{ marginBottom: "60px", textAlign: "justify" }}>
+          <p>
             Kumarans Model United Nations is a well reputed simulation of the
             United Nations and its other multi-lateral bodies where students
             assume roles of ambassadors of countries, individuals or any
             portfolios of power while deliberating, discussing and debating
             topics of global importance and relevance.
           </p>
-          <p style={{ textAlign: "justify", paddingBottom: "20px" }}>
+          <p>
             KMUN has its roots sown back in 2008 to encourage students to
             understand and find solutions to real world problems, not just those
             in textbooks.
           </p>
 
           <ButtonFilled text={linkText} as={Link} to={link} />
+        </div>
+        <div className="diamond-image">
+          <StaticImage
+            alt="HOC Committee"
+            src="../../../static/auditorium-1-masked.png"
+            layout="constrained"
+            placeholder="tracedSVG"
+            className="hello"
+          />
+          <div className="diamond-underlay">
+            <div className="diamond-bg"></div>
+          </div>
         </div>
       </div>
     </BasicTextModuleStyles>
